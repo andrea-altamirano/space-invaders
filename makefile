dca7ec2112be6a0abdceef70eb349fc4faef1286
andrea-altamirano <159966662+andrea-altamirano@users.noreply.github.com>
2024-04-17 00:22:19 +0000

    Version3

--- makefile
+++ makefile
@@ -1,12 +1,8 @@
-bin/test : src/tui_test.cpp
-	g++ src/tui_test.cpp -o bin/test -std=c++2a -lftxui-screen -lftxui-component -lftxui-dom
+bin/space : src/main.cpp
+	g++ src/main.cpp -o bin/space -std=c++2a -lftxui-screen -lftxui-component -lftxui-dom
 
-ejecutar : bin/test
-	./bin/test
+run : bin/space
+	./bin/space
 
-hola : receta2
-	echo hola mundo
 
-receta2 :
-	echo receta
 	
--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,23 @@
 #include <iostream>
+#include <string>
+#include <ftxui/dom/elements.hpp>
+#include <ftxui/screen/screen.hpp>
+
+using namespace std;
+using namespace ftxui;
 
 int main(int argc, char const *argv[])
 {
-    std::cout<<"Space invaders"<<std::endl;
-    return 0;
+    Element lienzo = hbox({
+        spinner(21,1) | bold
+
+    });
+
+    Screen pantalla = Screen::Create(
+        Dimension::Full(),
+        Dimension::Full());
+
+    Render(pantalla,lienzo);
+    pantalla.Print();
+    return EXIT_SUCCESS;
 }
