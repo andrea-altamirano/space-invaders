515ef32f58f1f29373d1ada7034d3b63cbc8167b
andrea-altamirano <159966662+andrea-altamirano@users.noreply.github.com>
2024-05-01 00:14:40 +0000

    Version8

--- include/Dibujo.hpp
+++ include/Dibujo.hpp
@@ -1,3 +1,44 @@
 #pragma once
 #include <string>
+#include <list>
 #include <ftxui/screen/screen.hpp>
+class Dibujo
+{
+    private:
+    int posicionX;
+    int posicionY;
+    list<string> contenido;
+    public:
+    Dibujo(){
+        this->posicionX = 0;
+        this->posicionY = 0;
+    }
+    Dibujo(int posicionX, int posicionY){
+        this->posicionX;
+        this->posicionY;
+    }
+
+    Dibujo(list<string> contenido, int posicionX, int posicionY){
+        this->contenido = contenido;
+        this->posicionX = posicionX;
+        this->posicionY = posicionY;
+    }
+
+    void Dibujar(ftxui::Screen Pantalla){
+        int posicionPalabraX = 0;
+            int posicionPalabraY = 0;
+            for (auto &&letra : contenido)
+            {
+                int posicionFinalX = posicionInicialX + posicionPalabraX;
+                int posicionFinalY = posicionInicialY + posicionPalabraY;
+
+                Pantalla.PixelAt(
+                    posicionFinalX,
+                    posicionFinalY
+                ).character = letra;
+                posicionPalabraX++;
+            }
+    }
+    ~Dibujo(){}
+
+}
--- src/ventana.cpp
+++ src/ventana.cpp
@@ -1,60 +1,22 @@
 #include <iostream>
-#include <string>
-#include <ftxui/dom/elements.hpp>
-#include <ftxui/screen/screen.hpp>
+#include <ftxui/component/component.hpp>
+#include <ftxui/component/screen_interactive.hpp>
+#include <Dibujo.hpp>
 #include <thread>
-
+#include <list>
+#include <experimental/random>
 using namespace std;
 using namespace ftxui;
 
+
 int main(int arg, char const *argv[])
 {
-    string palabra = "DVD";
-    string palabra1 = "bbbbbbbbbb";
-    int posicionInicialX = 5;
-    int posicionInicialY = 0;
 
-    int posicionInicial2X = 6;
-    int posicionInicial2Y = 1;
-
-    auto Pantalla = Screen::Create(Dimension::Full(), Dimension::Full());
-
-    while(true){
-        this_thread::sleep_for(0.5s);
-        Pantalla.Clear();
-
-        int posicionPalabraX = 0;
-        int posicionPalabraY = 0;
-
-        int posicionPalabra1X = 1;
-        int posicionPalabra1Y = 1;
-        for (auto &&letra : palabra)
-        {
-            int posicionFinalX= posicionInicialX + posicionPalabraX;
-            int posicionFinalY= posicionInicialY + posicionPalabraY;
-
-            Pantalla.PixelAt(posicionFinalX, posicionFinalY).character = letra;
-            posicionPalabraX++;
-
-        }
-                for (auto &&letra : palabra1)
-        {
-            int posicionFinal2X= posicionInicial2X + posicionPalabra1X;
-            int posicionFinal2Y= posicionInicial2Y + posicionPalabra1Y;
-
-            Pantalla.PixelAt(posicionFinal2X, posicionFinal2Y).character = letra;
-            posicionPalabra1X++;
-        }
-        Pantalla.Print();
-        Pantalla.Clear();
-        cout<<Pantalla.ResetPosition();
-
-        posicionInicialX++;
-        posicionInicialY++;
-
-        posicionInicial2X++;
-        posicionInicial2Y++;
+    list<Dibujo> dibujos;
+    for(int i=0; 1< 20; i++)
+    {
+        Dibujo contenido("Hola",0,0);
+        dibujos
     }
-
     return 0;
 }
